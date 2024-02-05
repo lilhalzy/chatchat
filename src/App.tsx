b8fb6955f4f3837f0126dd491c0725aa3cc6756a
lilhalzy <lilhalzy11@gmail.com>
2024-02-05 21:21:25 +0800

    initial commit
    boilerplate for React + Typescript + Tailwind + SWC + Vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return ( 
+    <div className='bg-amber-200'>
+      <h1 className="flex items-center justify-center h-screen text-3xl font-bold underline italic text-slate-500">
+      React + Typescript + Tailwind
+      </h1>
+      <p className='flex items-center justify-center text-slate-600 border-red-500'>Compiled with SWC and Vite</p>
+    </div>
+  )
+}
+
+export default App
