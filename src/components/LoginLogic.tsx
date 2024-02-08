728da348adb049bfe4d75210312834fd6321f814
lilhalzy <lilhalzy11@gmail.com>
2024-02-08 19:22:34 +0800

    added register ui

--- src/components/LoginLogic.tsx
+++ src/components/LoginLogic.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import Input from './Input';
+import React, { useState } from 'react';
 import Button from './Button';
+import Input from './Input';
 
 const LoginLogic = () => {
+  const [login, setLogin] = useState(true);
+
   return (
     <div className='w-full md:w-[450px]'>
       <h1 className='text-black text-center text-5xl mb-8 select-none font-title'>
-        Login
+        {login ? 'Login' : 'Register'}
       </h1>
       <div className='flex flex-col gap-3 bg-xWhite px-10 py-10 min-h-[150px] md:text-xl rounded-xl drop-shadow-md'>
         <Input name='username' type='text' />
         <Input name='password' type='password' />
-        <Button loading />
-        <Button text='Register' secondary loading />
+        {!login && <Input name='confirm-password' type='password' />}
+
+        {login ? (
+          <>
+            <Button />
+            <Button onClick={() => setLogin(false)} text='Register' secondary />
+          </>
+        ) : (
+          <>
+            <Button text='Register'/>
+            <Button onClick={() => setLogin(true)} text='Login' secondary />
+          </>
+        )}
       </div>
       <div className='flex justify-center mt-8 select-none drop-shadow-sm'>
         <p className='text-slate-300 italic hover:text-slate-400 transition-all'>
