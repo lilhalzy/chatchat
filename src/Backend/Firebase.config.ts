ecea1c603d77ab2257ba464e48a50f1cf3dc7926
lilhalzy <lilhalzy11@gmail.com>
2024-02-09 20:52:35 +0800

    initialize firebase (auth and cloud)
    safely stored sensitive info in env

---
+++ src/Backend/Firebase.config.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { configDotenv } from "dotenv";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore
+const db = getFirestore(app)
+const auth = getAuth() //working with authentication
+
+export {db, auth}
