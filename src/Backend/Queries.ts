213afb1bf94d9380d34d1c9cc435f06f17fd2a2e
lilhalzy <lilhalzy11@gmail.com>
2024-02-12 09:56:50 +0800

    feat: add toast notification

--- src/Backend/Queries.ts
+++ src/Backend/Queries.ts
@@ -1,6 +1,6 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-
-import { auth } from "./Firebase.config";
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { auth } from './Firebase.config';
 
 export const signUp = (data: {
   email: string;
@@ -8,12 +8,23 @@ export const signUp = (data: {
   confirmPassword: string;
 }) => {
   const { email, password, confirmPassword } = data;
-    
+
   if (email && password) {
     if (password === confirmPassword) {
-        createUserWithEmailAndPassword(auth, email, password).then(userCred => {
-            console.log(userCred);
-        })
-    } else console.log("password must match");
-  } else console.log('Make sure to fill in your stuff');
+      createUserWithEmailAndPassword(auth, email, password).then((userCred) => {
+        console.log(userCred);
+      });
+    } else console.log('password must match');
+  } else {
+    toast.error(`Don't be silly, put something in there`, {
+      position: "top-right",
+      autoClose: 1350,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+  }
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import App from './App.tsx'
 import { store } from './Redux/store.ts'
 import './index.css'
 
@@ -9,6 +11,18 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={1450}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        />
     </Provider>
   </React.StrictMode>,
 )
