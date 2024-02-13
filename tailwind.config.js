86f11365a6a04e8f0c26e8d1fcb3e2448103e6b2
lilhalzy <lilhalzy11@gmail.com>
2024-02-13 10:38:03 +0800

    chore: update screen lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    screens: {
-      sm: '320px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
     extend: {
       colors: {
         xGreen: '#426B1F',
